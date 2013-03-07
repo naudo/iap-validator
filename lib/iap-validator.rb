6f7ff1cb86e067981ef55bc6521011e17e3bdfec
Nicholas Audo <naudo@naudo.de>
2013-03-07 11:28:46 -0800

    include response body in exception

--- lib/iap-validator.rb
+++ lib/iap-validator.rb
@@ -2,6 +2,7 @@ require "iap-validator/version"
 
 require 'httparty'
 require 'exceptions'
+
 module IAPValidator
   class IAPValidator
     include HTTParty
@@ -36,30 +37,27 @@ module IAPValidator
       base_uri PRODUCTION_URL if production
       resp = post('/verifyReceipt', :body => MultiJson.encode({'receipt-data' => data, 'password' => itunes_connect_secret}))
 
+      response_body = MultiJson.decode(resp.body())
       case resp['status']
       when 21000
-        raise Exceptions::InvalidJSONError
+        raise Exceptions::InvalidJSONError(:message => response_body)
       when 21002
-        raise Exceptions::MalformedReceiptDataError
+        raise Exceptions::MalformedReceiptDataError(:message => response_body)
       when 21003
-        raise Exceptions::InvalidReceiptAuthenticationError
+        raise Exceptions::InvalidReceiptAuthenticationError(:message => response_body)
       when 21004
-        raise Exceptions::InvalidSharedSecretError
+        raise Exceptions::InvalidSharedSecretError(:message => response_body)
       when 21005
-        raise Exceptions::ReceiptServerUnavailableError
+        raise Exceptions::ReceiptServerUnavailableError(:message => response_body)
       when 21006
-        raise Exceptions::ExpiredSubscriptionError
+        raise Exceptions::ExpiredSubscriptionError(:message => response_body)
       when 21007
-        raise Exceptions::SanboxReceiptInProductionError
+        raise Exceptions::SanboxReceiptInProductionError(:message => response_body)
       when 21008
-        raise Exceptions::ProductionReceiptInSandboxError
+        raise Exceptions::ProductionReceiptInSandboxError(:message => response_body)
       end
 
-      if resp.code == 200
-        MultiJson.decode(resp.body())
-      else
-        nil
-      end
+     resp.code == 200 ? response_body : nil
     end
 
     def self.valid_subscription?(data, itunes_connect_secret, production = false)
